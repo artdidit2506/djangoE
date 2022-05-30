4044236cf0f0d31152fab54ecfd64abad94edcae
artdidit2506 <artdidit2506@gmail.com>
2022-05-31 00:59:00 +0700

    update data di templates

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'base/home.html')
+def index(request):
+    return render(request, 'base/index.html')
 def about(request):
     return render(request, 'base/about.html')
