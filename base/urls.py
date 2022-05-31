e66d018c08d3c4312fa30a62081e2755ef620f87
artdidit2506 <artdidit2506@gmail.com>
2022-05-31 14:25:38 +0700

    ganti file logo ke about

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('logo/', views.logo, name='logo'),
+    path('about/', views.about, name='about'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
-def logo(request):
-    return render(request, 'base/logo.html')
+def about(request):
+    return render(request, 'base/about.html')
