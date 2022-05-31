25504ccfe8ecc3ac1b70ea714c9a6733d73d7914
artdidit2506 <artdidit2506@gmail.com>
2022-05-31 14:22:47 +0700

    modified dan tambah file di base dan templates

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('logo/', views.logo, name='logo'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
-def about(request):
-    return render(request, 'base/about.html')
+def logo(request):
+    return render(request, 'base/logo.html')
